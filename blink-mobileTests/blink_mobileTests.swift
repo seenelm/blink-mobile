853f9c7d4e983c89ab12e460d485b4885257d62c
noahgross1 <noahgross1@gmail.com>
2025-07-22 22:18:50 -0400

    Initial Commit

---
+++ blink-mobileTests/blink_mobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  blink_mobileTests.swift
+//  blink-mobileTests
+//
+//  Created by Noah Gross on 7/22/25.
+//
+
+import Testing
+@testable import blink_mobile
+
+struct blink_mobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
